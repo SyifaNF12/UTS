9f2eab3070fd6cea9fe80765c3be84962f358db4
SyifaNF12 <syifanurfauziah472@gmail.com>
2016-04-18 23:01:27 +0700

    tugas

---
+++ Array.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author axioo
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        String [] b = new String [10];
+        
+        b[0]="1.anggur";
+        b[1]="2.mangga";
+        b[2]="3.apel";
+        b[3]="4.jambu";
+        b[4]="5.semangka";
+        b[5]="6.melon";
+        b[6]="7.kiwi";
+        b[7]="8.durian";
+        b[8]="9.rambutan";
+        b[9]="10.nanas";
+        
+        System.out.println(a);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        System.out.println(b[3]);
+        System.out.println(b[4]);
+        System.out.println(b[5]);
+        System.out.println(b[6]);
+        System.out.println(b[7]);
+        System.out.println(b[8]);
+        System.out.println(b[9]);
+        System.out.println(b.length);       
+    }
+    
+}
