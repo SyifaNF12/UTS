a7a88f0b0182eff5874a516ed8b76867d3933b8a
SyifaNF12 <syifanurfauziah472@gmail.com>
2016-04-18 23:04:08 +0700

    tugas

---
+++ Array2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array2;
+
+/**
+ *
+ * @author axioo
+ */
+public class Array2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,5};
+        String [] b = {"anggur", "mangga", "apel", "jambu", "semangka"};
+        
+       System.out.println(a[0]);
+       System.out.println(b[0]);
+       System.out.println(a[1]);
+       System.out.println(b[1]);
+       System.out.println(a[2]);
+       System.out.println(b[2]);
+       System.out.println(a[3]);
+       System.out.println(b[3]);
+       System.out.println(a[4]);
+       System.out.println(b[4]);
+       
+    }
+    
+}
